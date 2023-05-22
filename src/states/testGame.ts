1a075a8fd571fcb9e0f71c66299dc9f0c5667428
IanCanellas <128611453+IanCanellas@users.noreply.github.com>
2023-05-22 14:15:31 +0000

    Add Test Level
    Adds a (completeable) mockup level for future testing. Just a bunch of
    createPlatform() platforms put together into somethign coherent, but it
    should do the trick. Also adds some notes about screen and tile
    dimensions.

--- src/states/testGame.ts
+++ src/states/testGame.ts
@@ -43,6 +43,14 @@ export class TestGame extends GameState {
       const tex = await ImageTexture.createFromImage(await ResourceLoader.getImage(RightNormalV3), RightNormalV3);
 
       this.objectTree.addGameObjects([
+
+        /* One tile = 128
+           Current dimensions: 1920 x 1087 (15 x ~8.5 tiles)
+           (1024)
+
+           Probably a good idea to change resolution to 16:9 sometime
+        */
+
         new Player()
           .addChild(new AABB(Vector2.zero(), new Vector2(128, 128), true, "playerCollider"))
           .addChild(new TextureRect(Vector2.zero(), new Vector2(128, 128), tex, "playerTexture")),
@@ -51,10 +59,21 @@ export class TestGame extends GameState {
             .addChild(new AABB(Vector2.zero(), new Vector2(128, 128), true, "squarePlayerCollider"))
             .addChild(new ColorRect(Vector2.zero(), new Vector2(128, 128), "#00ff00", "squarePlayerTexture")),
 
-        await this.createPlatform(640, 600, 512, 320, 0.8, "wall", "wallTex"),
-
-        await this.createPlatform(0, 128, 1920, 128, 0.8, "ground", "groundTexture"),
-      ]);
+        await this.createPlatform(0, 1024, 128, 1024, 0.8, "ground", "groundTexture"),
+        await this.createPlatform(128, 128, 128, 128, 0.8, "ground", "groundTexture"),   
+        // move all this left by 128
+        // ^ ??? what
+        await this.createPlatform(256, 1024, 128, 640, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(256, 1088, 1408, 64, 0.8, "ground", "groundTexture"), 
+        await this.createPlatform(512, 384, 256, 256, 0.8, "ground", "groundTexture"), 
+        await this.createPlatform(768, 384, 128, 128, 0.8, "ground", "groundTexture"),
+        await this.createPlatform(512, 128, 1024, 128, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(1408, 896, 128, 896, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(384, 1024, 768, 128, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(384, 896, 256, 128, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(1024, 896, 128, 64, 0.8, "ground", "groundTexture"),   
+        await this.createPlatform(1152, 1024, 128, 768, 0.8, "ground", "groundTexture"), 
+        await this.createPlatform(1024, 384, 128, 128, 0.8, "ground", "groundTexture"),             ]);
     }
   
     override draw() {
