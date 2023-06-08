c15ceecdab62bd7174c291c64992532e901699fa
MageFlight <112671607+MageFlight@users.noreply.github.com>
2023-06-08 18:56:35 +0000

    Add the ability to create disabled togglePlatform
    Add a parameter in the TogglePlatform constructor that defines whether
    it should start out as disabled. The colliding shape's enablement MUST
    match the enablement in the TogglePlatform constructor.

--- src/togles/togglePlatform.ts
+++ src/togles/togglePlatform.ts
@@ -5,15 +5,20 @@ import { Utils } from "merlin-game-engine/dist/utils";
 export class TogglePlatform extends StaticBody {
     private toggleIndex: number;
 
-    constructor(position: Vector2, size: Vector2, toggleIndex: number, name: string) {
+    constructor(position: Vector2, size: Vector2, toggleIndex: number, isToggled: boolean, name: string) {
         super(position, size, 0b1, 0b1, 0.8, name);
         this.toggleIndex = toggleIndex;
-        Utils.listen("togglePlatform", (index: number) => {
-            if (index !== toggleIndex) return;
+        this.visible = isToggled;
 
-            const collider = this.getChildrenType<AABB>(AABB)[0];
-            collider.setVisible(!collider.isVisible());
-            this.visible = !this.visible;
+        Utils.listen("togglePlatform", (index: number) => {
+            if (index !== this.toggleIndex) return;
+
+            this.toggle(!this.visible);
         });
+    }
+
+    protected toggle(desiredState: boolean) {
+        this.getChildrenType<AABB>(AABB)[0].setVisible(desiredState);
+        this.visible = desiredState;
     }
 }
